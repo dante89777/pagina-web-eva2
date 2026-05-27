2f8ee3c9a04cacca6eb745152f01bc216b5dd383
dante89777 <dantes.burgos@inacapmail.cl>
2026-05-26 23:22:15 -0400

    commit2-script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// arreglo para guardar datos
+//Arreglo (tipo JSON en memoria)
 let colaboradores = [];
-// validar datos
+
+//Validación
 function validar(nombre, apellido, cargo, correo) {
+
   if (nombre === "" || apellido === "" || cargo === "" || correo === "") {
     return "Todos los campos son obligatorios";
   }
+
   if (!correo.includes("@empresa.cl")) {
-    return "El correo debe ser @empresa.cl";
+    return "Debe ser correo @empresa.cl";
   }
+
   return "";
 }
-// registrar colaborador
+
+//Registrar
 function registrar() {
 
   let nombre = document.getElementById("nombre").value;
@@ -25,6 +30,7 @@ function registrar() {
     return false;
   }
 
+  //Objeto tipo JSON
   let colaborador = {
     id: Date.now(),
     nombre: nombre,
@@ -32,7 +38,47 @@ function registrar() {
     cargo: cargo,
     correo: correo
   };
+
+  //Guardar en arreglo
   colaboradores.push(colaborador);
-  document.getElementById("error").innerHTML = "Registrado correctamente";
+
+  renderizar();
+
   return false;
+}
+//Renderizar tabla
+function renderizar(lista = colaboradores) {
+
+  let tabla = document.getElementById("tabla");
+  tabla.innerHTML = "";
+
+  lista.forEach(c => {
+    tabla.innerHTML += `
+      <tr>
+        <td>${c.nombre}</td>
+        <td>${c.apellido}</td>
+        <td>${c.cargo}</td>
+        <td>${c.correo}</td>
+        <td>
+          <button onclick="eliminar(${c.id})">Eliminar</button>
+        </td>
+      </tr>
+    `;
+  });
+}
+//Eliminar
+function eliminar(id) {
+  colaboradores = colaboradores.filter(c => c.id !== id);
+  renderizar();
+}
+//Filtrar
+function filtrar() {
+  let texto = document.getElementById("buscar").value.toLowerCase();
+
+  let resultado = colaboradores.filter(c =>
+    c.nombre.toLowerCase().includes(texto) ||
+    c.cargo.toLowerCase().includes(texto)
+  );
+
+  renderizar(resultado);
 }
